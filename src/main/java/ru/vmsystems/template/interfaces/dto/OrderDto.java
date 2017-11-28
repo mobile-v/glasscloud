63ead0bc37ac2130aa9035ccd5384a6d6596dbca
Vladimir Minikh <minih@omnicomm.ru>
2017-11-28 22:44:01 +0300

    swagger

--- src/main/java/ru/vmsystems/template/interfaces/dto/OrderDto.java
+++ src/main/java/ru/vmsystems/template/interfaces/dto/OrderDto.java
@@ -1,21 +1,36 @@
 package ru.vmsystems.template.interfaces.dto;
 
+import io.swagger.annotations.ApiModelProperty;
+
 import java.util.List;
 
 public class OrderDto {
     private Long id;
+    @ApiModelProperty(notes = "Номер заказа", required = true)
     private String number;
+    @ApiModelProperty(notes = "Описание")
     private String desc;
+    @ApiModelProperty(notes = "---- Номер счета. скорее всего не нужен ---")
     private String accountNumber;
+    @ApiModelProperty(notes = "Скидка", required = true)
     private String discount;
+    @ApiModelProperty(notes = "Сумма скидки", required = true)
     private String discountSum;
+    @ApiModelProperty(notes = "Количество", required = true)
     private Short count;
+    @ApiModelProperty(notes = "Сумма", required = true)
     private String summa;
+    @ApiModelProperty(notes = "Площадь", required = true)
     private Double area;
+    @ApiModelProperty(notes = "Периметр", required = true)
     private Double perimeter;
+    @ApiModelProperty(notes = "Заказчик", required = true)
     private ClientDto client;
+    @ApiModelProperty(notes = "Точка приема заказов", required = true)
     private ReceptionOfOrderDto receptionOfOrder;
+    @ApiModelProperty(notes = "Дата создания")
     private String creationDate;
+    @ApiModelProperty(notes = "Дата обновления")
     private String updateDate;
 
     private List<OrderItemDto> items;
